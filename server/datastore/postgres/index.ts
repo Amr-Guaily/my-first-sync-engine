89fb1c6d5bc22422407dfe6ddea3e5a341d219bf
Amr-Guaily <amrguaily@gmail.com>
2025-09-14 10:41:22 +0300

    ✨ Add PostgreSQL connection pool implementation

---
+++ server/datastore/postgres/index.ts
@@ -0,0 +1,10 @@
+import { Pool } from 'pg';
+
+// Export a singleton pg pool instance => All DAOs can import this connection to run queries
+export const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
